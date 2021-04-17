bb1ef69a56b2abf939c857b0a444fecefa049e46
harshmittal2210 <harshmittal2210@gmail.com>
2021-04-18 03:45:06 +0530

    UI changed

--- Qt/networkPlot/plotnetworkdata.cpp
+++ Qt/networkPlot/plotnetworkdata.cpp
@@ -13,3 +13,13 @@ plotNetworkData::~plotNetworkData()
     delete ui;
 }
 
+
+void plotNetworkData::on_plotTypeComboBox_currentIndexChanged(int index)
+{
+
+}
+
+void plotNetworkData::on_lineStyleComboBox_currentIndexChanged(int index)
+{
+
+}
