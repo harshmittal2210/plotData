281656f7554b17279e71eb11b997e30404a27f8f
harshmittal2210 <harshmittal2210@gmail.com>
2021-04-26 03:05:48 +0530

    Server added in main

--- Qt/networkPlotServer/main.cpp
+++ Qt/networkPlotServer/main.cpp
@@ -1,11 +1,15 @@
-#include "include/plotnetworkdata.h"
+#include "plotnetworkdata.h"
 
 #include <QApplication>
+#include "myserver.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     plotNetworkData w;
     w.show();
+    // Make a server and starts it
+    MyServer server;
+    server.startServer();
     return a.exec();
 }
