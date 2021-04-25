9e820388856892f101b13a67255dd8cb2a7911a7
harshmittal2210 <harshmittal2210@gmail.com>
2021-04-26 03:09:43 +0530

    Simple Client added

---
+++ Qt/networkPlotServer/pythonClient/client.py
@@ -0,0 +1,12 @@
+import socket                   # Import socket module
+
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+port = 1234                   # Reserve a port for your service.
+
+s.connect((host, port))
+s.send("Hello server!".encode())
+
+
+s.close()
+print('Connection Closed')
