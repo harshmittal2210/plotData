429ba1d328c7ae120b640cf6a65952d4d4b3a944
harshmittal2210 <harshmittal2210@gmail.com>
2021-04-26 02:54:11 +0530

    Thread code added

--- Qt/networkPlotServer/include/mythread.h
+++ Qt/networkPlotServer/include/mythread.h
@@ -3,11 +3,28 @@
 
 #include <QThread>
 
+#include <QThread>
+#include <QTcpSocket>
+#include <QDebug>
+
 class MyThread : public QThread
 {
     Q_OBJECT
 public:
-    MyThread();
+    explicit MyThread(qintptr ID, QObject *parent = 0);
+
+    void run();
+
+signals:
+    void error(QTcpSocket::SocketError socketerror);
+
+public slots:
+    void readyRead();
+    void disconnected();
+
+private:
+    QTcpSocket *socket;
+    qintptr socketDescriptor;
 };
 
 #endif // MYTHREAD_H
